f48d12bf19569f176e55e046f486bfd478c2ca14
James Li <james.li@lxd.org.cn>
2021-01-06 10:52:37 +0800

    init

---
+++ READ.md
@@ -0,0 +1,11 @@
+# Wiki
+
+- 用于发现和更新IP地址服务
+
+
+# License
+
+Copyright 2021 James Li.
+Licensed under the Apache License,
+Version 2.0:
+http://www.apache.org/licenses/LICENSE-2.0
---
+++ ip.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+//IP 全局变量，用于保存IP，初始值是字符串null
+var IP string = "null"
+
+//GetIP 用于获取IP
+func GetIP(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, IP)
+	log.Println(strings.Split(r.RemoteAddr, ":")[0])
+}
+
+//SetIP 用于更新IP
+func SetIP(w http.ResponseWriter, r *http.Request) {
+	IP = strings.Split(r.RemoteAddr, ":")[0]
+	fmt.Fprintln(w, "Successful")
+	log.Printf("IP Address Update Success,Current IP [%s] \n", IP)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "0.0.0.0:8080",
+	}
+	http.HandleFunc("/getip", GetIP)
+	http.HandleFunc("/setip", SetIP)
+	server.ListenAndServe()
+}
